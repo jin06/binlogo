b591f4f041cda992937adb7119a11f012c563ff8
jinlong <jinlong4696@163.com>
2021-11-23 14:36:13 +0800

    v1.0.14

--- configs/config.go
+++ configs/config.go
@@ -6,7 +6,9 @@ import (
 
 // ENV global environment
 var ENV Env
+
 // NodeName current node's name
 var NodeName string
+
 // NodeIP current node's ip
 var NodeIP net.IP
--- configs/version.go
+++ configs/version.go
@@ -1,4 +1,4 @@
 package configs
 
 // VERSITON version of binlogo
-const VERSITON = "1.0.13"
+const VERSITON = "1.0.14"
