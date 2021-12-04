05c35c898f01807d0eef0b9cc82f89def7abeac5
Caesar <jinlong4696@163.com>
2021-12-04 11:32:32 +0800

    1.0.24

--- configs/version.go
+++ configs/version.go
@@ -1,4 +1,4 @@
 package configs
 
 // VERSITON version of binlogo
-const VERSITON = "1.0.23"
+const VERSITON = "1.0.24"
--- examples/redis/main.go
+++ examples/redis/main.go
@@ -11,6 +11,7 @@ func main() {
 	list := "test-redis"
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "localhost:16379",
+		//Addr:     "redis-apple-master.shan-dev.svc.cluster.local:6379",
 		Password: "",
 		DB:       0,
 	})
