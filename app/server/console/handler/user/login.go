304de3678eb33a8993c9233e39a13ca591d55ae9
jinlong <jinlong4696@163.com>
2023-12-12 14:21:57 +0800

    api user login auth and token store; ldap implement redis store

--- app/server/console/handler/user/login.go
+++ app/server/console/handler/user/login.go
@@ -1,16 +1,55 @@
 package user
 
-import "github.com/gin-gonic/gin"
-
-type AuthType string
-
-const (
-	Basic = "basic"
-	Ldap  = "ldap"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jin06/binlogo/app/server/console/handler"
+	"github.com/jin06/binlogo/app/server/console/service"
 )
 
-type Handler interface {
-	Login(c *gin.Context)
-	Logout(c *gin.Context)
-	Info(c *gin.Context)
+type token struct {
+	Roles        []string `json:"roles"`
+	Introduction string   `json:"introduction"`
+	Avatar       string   `json:"avatar"`
+	Name         string   `json:"name"`
+}
+
+type loginReq struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func Login(c *gin.Context) {
+	var req loginReq
+	if err := c.BindJSON(&req); err != nil {
+		c.JSON(200, handler.Fail(err))
+		return
+	}
+	pass := service.DefaultAuth().Verify(req.Username, req.Password)
+	if !pass {
+		c.JSON(200, handler.FailCode(handler.CodeBadUsernameOrPassword))
+		return
+	}
+	t := service.DefaultStore().Set()
+	c.JSON(200, handler.Success(map[string]string{"token": t}))
+}
+
+func Logout(c *gin.Context) {
+	t := c.GetHeader("x-token")
+	service.DefaultStore().Remove(t)
+	c.JSON(200, handler.Success(nil))
+}
+
+func Info(c *gin.Context) {
+	t := c.Query("token")
+	if service.DefaultStore().Get(t) {
+		c.JSON(200, handler.Success(token{
+			Roles:        []string{"admin"},
+			Introduction: "I am a super administrator",
+			Avatar:       "https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif",
+			Name:         "Super Admin",
+		}))
+		return
+	}
+	c.JSON(200, handler.FailCode(handler.CodeTokenExpired))
+	return
 }
--- go.mod
+++ go.mod
@@ -27,8 +27,13 @@ require (
 )
 
 require (
+	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.5 // indirect
+	github.com/go-ldap/ldap/v3 v3.4.6 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/google/uuid v1.4.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/redis/go-redis/v9 v9.3.0 // indirect
 )
 
 require (
@@ -37,7 +42,7 @@ require (
 	github.com/andybalholm/brotli v1.0.2 // indirect
 	github.com/armon/go-metrics v0.0.0-20180917152333-f0300d1749da // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
@@ -114,10 +119,10 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.17.0 // indirect
-	golang.org/x/crypto v0.0.0-20210920023735-84f357641f63 // indirect
-	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
-	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto v0.0.0-20210828152312-66f60bf46e71 // indirect
 	google.golang.org/grpc v1.42.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
