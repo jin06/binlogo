54e0b94c583918467956f6e55057aade4e26f28a
jinlong <jinlong4696@163.com>
2024-01-04 10:56:44 +0800

    edit router/index.js & remove dir views/example

--- dashboard/src/router/index.js
+++ dashboard/src/router/index.js
@@ -6,12 +6,6 @@ Vue.use(Router)
 /* Layout */
 import Layout from '@/layout'
 
-/* Router Modules */
-import componentsRouter from './modules/components'
-import chartsRouter from './modules/charts'
-import tableRouter from './modules/table'
-import nestedRouter from './modules/nested'
-
 /**
  * Note: sub-menu only appear when route children.length >= 1
  * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
